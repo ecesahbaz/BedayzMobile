8ee5d7af3c58125fbf40187a18d1d2289ba40600
ecesahbaz <ecesahbaz@outlook.com>
2020-05-24 04:55:07 +0300

    sign in and sign up

---
+++ xamarin7/Views/GirisSayfası.xaml.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace xamarin7.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class GirisSayfası : ContentPage
+    {
+        public GirisSayfası()
+        {
+            InitializeComponent();
+
+            image.Source= "https://www.bedayz.com/CustomCss/images/logo.png";
+        }
+
+       
+    }
+}
